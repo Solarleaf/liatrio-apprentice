aa1c78f530d69083f5711d80b29a184d12b4362f
Solarleaf <124109321+Solarleaf@users.noreply.github.com>
2024-04-18 00:06:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
         message: "My name is Leif Hasle",
         timestamp: new Date()
     };
-    res.json(responseObject);
+    res.status(200).json(responseObject);
 });
 
 app.listen(port, host, () => {
