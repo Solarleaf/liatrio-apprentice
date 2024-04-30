787ae81ceebd26aa7053a91c15ab616abdd71dc8
Solarleaf <124109321+Solarleaf@users.noreply.github.com>
2024-04-30 14:43:08 -0700

    ForgotToName

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-
+// Framework & includes
 const express = require("express");
 const http = require("http");
-
 const app = express();
-
+// Variables
 const port = 80;
 const hostname = '127.17.0.1';
-const mess_n = "My name is L. H.";
-
+const mess_n = "My name is Leif H.";
+// Routing
 app.get("/", (req, res) => {
     const responseObject = {
         message: mess_n,
         timestamp: new Date().getTime(),
         mini: mess_n.replace(/\s+/g, ''),
-        // timestamp: System.currentTimeMillis() / 1000,
     };
     res.status(200).json(responseObject);
 });
+// Start Server and binding
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
+
+
+// console.log(`Server running at http://${hostname}:${port}/`);
 /*
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}!`);
     // console.log(`Server running at http://${hostname}:${port}/`);
 });
 */
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`);
-    // console.log(`Server running at http://${hostname}:${port}/`);
-   //
-});
