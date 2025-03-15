af0dad908b7e96cf9dd2ac69e62d5bf494bf842e
Solar Leaf <lhasle@csuchico.edu>
2025-03-14 22:47:05 -0700

    Update index.js
    Add a filed to the JSON output of the application

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ const port = process.env.PORT || 80;
 // const hostname = '127.17.0.1';
 const hostname = '0.0.0.0';
 const mess_n = "My name is fun!";
+const funFacts = [
+    "Kubernetes Had a Secret Codename: Project Seven",
+    "Phippy & Friends have a childrens book about Kubernetes",
+    "Octopuses have three hearts.",
+    "Kubernetes” is the Greek word for a ship’s captain. We get the words Cybernetic and Gubernatorial from it",
+    "Why don’t Kubernetes pods ever get lost? Because they always have a service to find them!",
+    "Why did the container apply for a job? It wanted better orchestration in its life!",
+    "What’s a Kubernetes cluster’s favorite type of music? Heavy metal… because it’s full of nodes!",
+    "How does a DevOps engineer like their coffee? With CI/CD (Caffeine Input / Caffeine Deployment)!",
+];
 // Routing that responds to a HTTP GET request
 app.get("/", (req, res) => {
     const responseObject = {
@@ -15,6 +25,7 @@ app.get("/", (req, res) => {
         timestamp: new Date().getTime(),
         // Minified
         mini: mess_n.replace(/\s+/g, ''),
+        funFact: funFacts[Math.floor(Math.random() * funFacts.length)],
     };
     res.status(200).json(responseObject);
 });
