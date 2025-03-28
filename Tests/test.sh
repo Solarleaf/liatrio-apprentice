ac554f93c86b9a4e5f28348117c673319aca7c62
Solar <lhasle@csuchico.edu>
2025-03-27 22:55:44 -0700

    Typo

--- Tests/test.sh
+++ Tests/test.sh
@@ -5,7 +5,7 @@ echo "Waiting for pod to be ready"
 kubectl wait --for=condition=Ready pod -l app=liatrio-app-api --timeout=60s
 echo ""
 
-echo ""Currling localhost"
+echo "Currling localhost"
 curl http://localhost:8080
 echo ""
 
