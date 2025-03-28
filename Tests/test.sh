2453f78022a989d41b9cf990a221fdd5770c76f8
Solar <lhasle@csuchico.edu>
2025-03-27 22:53:10 -0700

    Adding another test

--- Tests/test.sh
+++ Tests/test.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 set -e
 
-echo "⏳ Waiting for pod to be ready..."
+echo "Waiting for pod to be ready"
 kubectl wait --for=condition=Ready pod -l app=liatrio-app-api --timeout=60s
+echo ""
 
-echo "🚀 Pod is ready. Running curl test from inside the cluster..."
+echo ""Currling localhost"
+curl http://localhost:8080
+echo ""
+
+echo "Running curl test from inside the cluster."
 kubectl run curlpod --rm -i --tty --image=curlimages/curl --restart=Never -- \
   curl -s http://liatrio-service-api:8080
 
-echo "✅ Curl test passed!"
+echo ""
+
+echo "Curl test passed!"
