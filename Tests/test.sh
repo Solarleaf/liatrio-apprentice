e7647848488b027309b2b8f62b83e1283fab7076
Solar <lhasle@csuchico.edu>
2025-03-29 22:22:06 -0700

    ArgoCD using 8080

--- Tests/test.sh
+++ Tests/test.sh
@@ -6,12 +6,12 @@ kubectl wait --for=condition=Ready pod -l app=liatrio-app-api --timeout=60s
 echo ""
 
 echo "Currling localhost"
-curl http://localhost:8080
+curl http://localhost:8081
 echo ""
 
 echo "Running curl test from inside the cluster."
 kubectl run curlpod --rm -i --image=curlimages/curl --restart=Never -- \
-  curl -s http://liatrio-service-api:8080
+  curl -s http://liatrio-service-api:8081
 
 echo ""
 
